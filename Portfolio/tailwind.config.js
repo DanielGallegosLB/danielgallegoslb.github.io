484b06acc2b5879ed261f1eca3f5231e216e92c4
DanielGallegosLB <daniel.gallegoslb@gmail.com>
2024-09-03 18:38:30 -0300

    First Commit

---
+++ Portfolio/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#050304",
+        secondary: "#fff6e5",
+        tertiary: "#1d1836",
+        quaternary: "#e42e57",
+        
+
+        "black-100": "#100d25",
+        "black-200": "#090325",
+        "white-100": "#f3f3f3",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #211e35",
+      },
+      screens: {
+        xs: "450px",
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/src/assets/herobg.png')",
+      },
+    },
+  },
+  plugins: [],
+};

a0be4ea48d76d7b8b73f202b95d86392a94f1de9
DanielGallegosLB <daniel.gallegoslb@gmail.com>
2024-09-04 07:40:58 -0300

    fix 3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/DanielGallegosLB/39_Portfolio_Three.js',
+  base: 'https://github.com/DanielGallegosLB/39_Portfolio_Three.js/',
   plugins: [react()],
 })

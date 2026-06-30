d5f9879b588a5dc842786614356454e42f15c10e
DanielGallegosLB <daniel.gallegoslb@gmail.com>
2026-06-30 07:13:41 -0400

    remove scroll

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -52,23 +52,6 @@ const Hero = () => {
       </div>
 
       {/* Scroll indicator */}
-      <div className='absolute bottom-10 left-0 right-0 flex justify-center items-center'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
-            />
-          </div>
-        </a>
-      </div>
     </section>
   );
 };
